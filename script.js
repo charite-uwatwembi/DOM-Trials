49c19a6c19d500a8589bbe80b0bf983713aa5815
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-04-16 17:15:31 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// function changeAllArticlesColors(){
+//     let product = document.querySelector('#products')
+//     console.log(product)
+//     let article = product.querySelectorAll('article')
+
+//     for(let element of article){
+//         element.classList.add('sale')
+//     }
+    
+
+// }
+
+// changeAllArticlesColors()
+
+function attachBuyEvents(){
+
+}
+attachBuyEvents()
